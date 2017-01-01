c02a5fddfacce87d3e6dd1c5c003bf0b1105b739
Bing072 <bingoblog@163.com>
2017-01-01 15:09:03 +0800

    Add Update Class

---
+++ class.update.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 插件更新类
+ *
+ * @package Hermit X
+ * @since Hermit X 2.5.9
+ */
+
+/**
+ * 插件更新
+ *
+ * @since Hermit X 2.5.9
+ */
+final class Hermit_Update {
+
+}
+
+// End of page.
