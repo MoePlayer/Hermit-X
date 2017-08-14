175d2e69acb1fad1551fb6fb38a1c37224ec1005
METO <i@i-meto.com>
2017-08-14 14:11:49 +0800

    :fire: Add lyric_mod

--- class.hermit.php
+++ class.hermit.php
@@ -222,7 +222,7 @@ class hermit
 
                     //默认路由
                     default:
-                    $re = '/^(?<site>(netease|xiami|tencent|kugou|baidu)?)_?(?<scope>songs|songlist|album|playlist|collect|artist|song_url|pic_url|id_parse)$/i';
+                    $re = '/^(?<site>(netease|xiami|tencent|kugou|baidu)?)_?(?<scope>songs|songlist|album|playlist|collect|artist|song_url|pic_url|lrc_url|id_parse)$/i';
                     preg_match($re, $scope, $matches);
                         if (!empty($matches['scope'])) {
                             $scope = $matches['scope'];
@@ -248,7 +248,12 @@ class hermit
                                         'status' => 200,
                                         'msg' => $HMTJSON->$scope($site, explode(',', $_GET['src']))
                                     );
-                                } else {
+                                } elseif ($scope === 'lrc_url') {
+                                    $this->nonce_verify();
+                                    echo $HMTJSON->$scope($site, $_GET['id']);
+                                    exit;
+                                }
+                                else {
                                     $this->nonce_verify();
                                     $result = array(
                                         'status' => 200,
