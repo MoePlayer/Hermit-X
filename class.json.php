bb1250a35f5df55a2172286e1c3f26a1a38dcf3d
Fly3949 <i@Iifly.cn>
2021-01-23 16:25:24 +0000

    feat: Update URL replacing rules

--- class.json.php
+++ class.json.php
@@ -38,18 +38,19 @@ class HermitJson
         //     exit;
         // }
 
-        if ($site === "netease") {
-            $url = str_replace('http://m9.', 'http://m7.', $url);
-            $url = str_replace('http://m7c.', 'http://m7.', $url);
-            $url = str_replace('http://m8c.', 'http://m7.', $url);
-            $url = str_replace('http://m8.', 'http://m7.', $url);
-            $url = str_replace('http://m7.', 'https://m7.', $url);
-            $url = str_replace('http://m10', 'https://m10', $url);
-        }
-        if ($site === "xiami" || $site === 'tencent') {
+        if ($site == 'netease') {
+            $url = str_replace('://m7c.', '://m7.', $url);
+            $url = str_replace('://m8c.', '://m8.', $url);
             $url = str_replace('http://', 'https://', $url);
         }
-        if ($site === 'baidu') {
+        if ($site == 'tencent') {
+            $url = str_replace('http://', 'https://', $url);
+            $url = str_replace('ws.stream.qqmusic.qq.com', 'dl.stream.qqmusic.qq.com', $url);
+        }
+        if ($site == 'xiami') {
+            $url = str_replace('http://', 'https://', $url);
+        }
+        if ($site == 'baidu') {
             $url = str_replace('http://zhangmenshiting.qianqian.com', 'https://gss3.baidu.com/y0s1hSulBw92lNKgpU_Z2jR7b2w6buu', $url);
         }
 
